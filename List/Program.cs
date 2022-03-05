d3bbd678619e2044851501868309a2407f9a4d96
MrSampy <mynameisserzheo@gmail.com>
2022-03-05 19:19:15 +0200

    Creating class Element

--- List/Program.cs
+++ List/Program.cs
@@ -5,7 +5,12 @@
         public static void Main(string[] args)
         {
         }
-
+        public class Element<T>
+        {   public Element(T data) => _Data = data; 
+            public T _Data { set; get; }
+            public Element<T> _PrevEl { get; set; }
+            public Element<T> _NextEl { get; set; }
+        }
         
     }
 }   
